5e6eb735f836ccd38d32fe62b042be2be00bd8e1
Isaac Girard <isaac@isaacgirard.com>
2014-02-06 11:41:23 -0500

    fix Beats playback not recognized and album artwork not displayed

--- src/controllers/BeatsMusicController.js
+++ src/controllers/BeatsMusicController.js
@@ -7,7 +7,7 @@ controller = new BasicController({
     thumbsDown: true
   },
   playStateSelector: '#play_pause_icon',
-  playStateClass: '.icon-bicons_pause',
+  playStateClass: 'icon-bicons_pause',
   playPauseSelector: '#play_pause_icon',
   nextSelector: '#t-next',
   previousSelector: '#t-prev',
@@ -19,4 +19,19 @@ controller = new BasicController({
   isThumbsUpSelector: "#t-love.active",
   isThumbsDownSelector: "#t-hate.active",
 });
+
+// verify controller loaded
 console.log("Got the Beats controller");
+
+// grab url from background-image and upgrade to medium size
+controller.override('getAlbumArt', function() {
+  var art = document.getElementById("t-art");
+  var artURL = getComputedStyle(art).getPropertyValue('background-image').slice(4,-1);
+  var artMed = artURL.replace('small', 'medium');
+  return artMed;
+});
+
+// listen for click to recheck for play state
+document.body.addEventListener("click", function() {
+  setTimeout(sendState, 1000);
+});

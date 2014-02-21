1ce2b5b21ce5b995a68ecd357a69596d1f5b874c
Michael Feldstein <msfeldstein@gmail.com>
2014-02-21 10:31:58 -0800

    Fix Mixcloud

--- src/controllers/MixcloudController.js
+++ src/controllers/MixcloudController.js
@@ -1,43 +1,16 @@
-controller = {
-  init: function() {
-    document.body.addEventListener('DOMSubtreeModified', sendState);
-    return true;
-  },
-  name: "Mixcloud",
-  nextSong: function() {
-    clickById('jump-forward-button');
-  },
-  previousSong: function() {
-    clickById('jump-backward-button');
-  },
-  play: function() {
-    clickById('player-play');
-  },
+controller = new BasicController({
   supports: {
-    next:true,
-    previous:true,
-    playpause:true
+    playpause: true,
   },
-  isPlaying: function() {
-    var playbutton = document.getElementById('player-play');
-    return playbutton && playbutton.classList.contains('playing');
-  },
-  getAlbumArt: function() {
-    var img = document.getElementById('cloudcast-image');
-    return img && img.src;
-  },
-  getTitle: function() {
-    return querySelectorText('.now-playing-track-info .js-title');
-  },
-  getArtist: function() {
-    return querySelectorText('.now-playing-track-info .js-artist');
-  },
-  getState: function() {
-    var state = {};
-    state.playing = this.isPlaying();
-    state.albumArt = this.getAlbumArt();
-    state.title = this.getTitle();
-    state.artist = this.getArtist();
-    return state;
-  }
-};
+  playStateSelector: '.player-control',
+  playStateClass: 'pause-state',
+  playPauseSelector: '.player-control',
+  titleSelector: '.player-cloudcast-title',
+  artistSelector: '.player-cloudcast-author-link',
+  artworkImageSelector: '.player-cloudcast-image img',
+});
+
+controller.override('getAlbumArt', function(_super) {
+  var art = _super();
+  return art && art.replace(/\/60\//g, '\/300\/');
+})

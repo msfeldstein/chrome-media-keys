39451d9d3cd1541f37080af2da68cc315962e7af
Michael Feldstein <msfeldstein@gmail.com>
2015-01-21 17:51:01 -0800

    v3.71 fix rhapsody

--- src/controllers/RhapsodyController.js
+++ src/controllers/RhapsodyController.js
@@ -1,51 +1,24 @@
-controller = {
-    init: function() {
-        document.body.addEventListener("DOMSubtreeModified", sendState);
-        return true;
-    },
-    name: "Rhapsody",
-    supports: {
-        playpause: true,
-        next: true,
-        previous: true,
-        favorite: true
-    },
-    nextSong: function() {
-        clickById("player-next");
-    },
-    previousSong: function() {
-        clickById("player-previous");
-    },
-    favorite: function() {
-        clickById("player-favorite");
-    },
-    isPlaying: function() {
-      return document.querySelector("#player-play").style.display == "none";
-    },
-    play: function() {
-        if (this.isPlaying())
-            clickById("player-pause");
-        else
-            clickById("player-play");
-    },
-    albumArt: function() {
-        var arts = document.querySelectorAll(".player-album-thumbnail");
-        for (var i = 0; i < arts.length; ++i) {
-            var art = arts[i];
-            if (getComputedStyle(art).opacity == 1) {
-                return art.querySelector("img").src;
-            }
-        }
-        return "";
-    },
-    getState: function() {
-        var state = {};
-        state.playing = this.isPlaying();
-        var favoriteButton = document.querySelector("#player-favorite");
-        state.favorite = favoriteButton && favoriteButton.classList.contains("selected");
-        state.title = innerText("player-track");
-        state.artist = innerText("player-artist");
-        state.albumArt = this.albumArt();
-        return state;
-    }
-}
+document.body.addEventListener('DOMSubtreeModified', throttle(sendState, 250));
+
+controller = new BasicController({
+  supports: {
+    playpause: true,
+    next: true,
+    previous: true,
+    favorite: true
+  },
+  playPauseSelector: '.player-play-button *',
+  previousSelector: '.player-rewind-button',
+  nextSelector: '.player-advance-button',
+  titleSelector: '.player-info-row .player-track',
+  artistSelector: '.player-info-row .player-artist',
+  artworkImageSelector: '.player-album-thumbnail img',
+  isFavoriteSelector: '.favorite-button.active',
+  favoriteSelector: '.favorite-button',
+  playStateSelector: '.player-play-button',
+  watchedElements: ['.player-info-row']
+});
+
+controller.override('isPlaying', function() {
+    return !!document.querySelector('.player-play-button .icon-pause');
+});

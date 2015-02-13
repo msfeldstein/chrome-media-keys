7a5b872caf7f7cffde7451b762c398a2d9bc3de8
Michael Feldstein <msfeldstein@gmail.com>
2015-02-13 15:49:32 -0800

    Fix deezer

--- src/controllers/DeezerController.js
+++ src/controllers/DeezerController.js
@@ -10,11 +10,11 @@ controller = new BasicController({
   nextSelector: '.player-controls .control-next',
   previousSelector: '.player-controls .control-prev',
   titleSelector: '.player-track-title',
-  artistSelector: '.player-track-artist .player-track-link',
+  artistSelector: '.player-track .player-track-link',
   artworkImageSelector: '.player-cover img',
   watchedElements: ['.player-controls .icon-play']
 });
-
+ 
 controller.override('isPlaying', function() {
   return !!document.querySelector('.player-controls .control-pause');
 });

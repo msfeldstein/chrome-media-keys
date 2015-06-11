e8539151080680c3b8c96f3c0d9f6aca07dac3cc
Michael Feldstein <msfeldstein@gmail.com>
2015-06-11 15:02:13 -0700

    Try supporting both versions of spotify

--- src/controllers/SpotifyController.js
+++ src/controllers/SpotifyController.js
@@ -5,16 +5,16 @@ controller = new BasicController({
     previous: true
   },
   useLazyObserving: true,
-  frameSelector: '#main',
-  playStateSelector: '#play',
+  frameSelector: '#main, #app-player',
+  playStateSelector: '#play, #play-pause',
   playStateClass: 'playing',
-  playPauseSelector: '#play',
+  playPauseSelector: '#play, #play-pause',
   nextSelector: '#next',
   previousSelector: '#previous',
-  titleSelector: '.caption .track',
-  artistSelector: '.caption .artist'
+  titleSelector: '.caption .track, #track-name',
+  artistSelector: '.caption .artist, #track-artist'
 });
 
 controller.override('getAlbumArt', function() {
-  return document.querySelector(this.frameSelector).contentDocument.querySelector('#large-cover-image').style.backgroundImage.slice(4, -1);
+  return document.querySelector(this.frameSelector).contentDocument.querySelector('#large-cover-image, #cover-art .sp-image-img').style.backgroundImage.slice(4, -1);
 });

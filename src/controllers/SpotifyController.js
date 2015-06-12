b3d6d0ea244499c44eb0d3f8c50b1b4ddc3625f8
Eric Nemchik <eric@nemchik.com>
2015-06-11 20:31:07 -0500

    Support New and Old Spotify

--- src/controllers/SpotifyController.js
+++ src/controllers/SpotifyController.js
@@ -1,20 +1,43 @@
-controller = new BasicController({
-  supports: {
+if (!!document.querySelector('#app-player')) { // Old Player
+  controller = new BasicController({
+    supports: {
     playpause: true,
     next: true,
     previous: true
-  },
-  useLazyObserving: true,
-  frameSelector: '#main, #app-player',
-  playStateSelector: '#play, #play-pause',
-  playStateClass: 'playing',
-  playPauseSelector: '#play, #play-pause',
-  nextSelector: '#next',
-  previousSelector: '#previous',
-  titleSelector: '.caption .track, #track-name',
-  artistSelector: '.caption .artist, #track-artist'
-});
+    },
+    useLazyObserving: true,
+    frameSelector: '#app-player',
+    playStateSelector: '#play-pause',
+    playStateClass: 'playing',
+    playPauseSelector: '#play-pause',
+    nextSelector: '#next',
+    previousSelector: '#previous',
+    titleSelector: '#track-name',
+    artistSelector: '#track-artist'
+  });
 
-controller.override('getAlbumArt', function() {
-  return document.querySelector(this.frameSelector).contentDocument.querySelector('#large-cover-image, #cover-art .sp-image-img').style.backgroundImage.slice(4, -1);
-});
+  controller.override('getAlbumArt', function() {
+    return document.querySelector(this.frameSelector).contentDocument.querySelector('#cover-art .sp-image-img').style.backgroundImage.slice(4, -1);
+  });
+} else { // New Player
+  controller = new BasicController({
+    supports: {
+    playpause: true,
+    next: true,
+    previous: true
+    },
+    useLazyObserving: true,
+    frameSelector: '#main',
+    playStateSelector: '#play',
+    playStateClass: 'playing',
+    playPauseSelector: '#play',
+    nextSelector: '#next',
+    previousSelector: '#previous',
+    titleSelector: '.caption .track',
+    artistSelector: '.caption .artist'
+  });
+
+  controller.override('getAlbumArt', function() {
+    return document.querySelector(this.frameSelector).contentDocument.querySelector('#large-cover-image').style.backgroundImage.slice(4, -1);
+  });
+}

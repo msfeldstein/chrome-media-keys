134d9ee7fea2a3d12e5a8c221e09442533f4b512
Michael Feldstein <msfeldstein@gmail.com>
2015-06-13 00:17:02 -0700

    Fix Songza

--- src/controllers/SongzaController.js
+++ src/controllers/SongzaController.js
@@ -3,21 +3,13 @@ controller = new BasicController({
     playpause: true,
     next: true
   },
+  doThrottling: true,
   playPauseSelector: '.miniplayer-control-play-pause',
   nextSelector: '.miniplayer-control-skip',
   titleSelector: '.miniplayer-info-track-title',
   artistSelector: '.miniplayer-info-artist-name',
   playStateSelector: '.player-state-song',
   playStateClass: 'player-state-play',
-  artworkImageSelector: '.miniplayer-album-art'
+  artworkImageSelector: '.miniplayer-album-art',
+  watchedElements: ["#player"]
 });
-
-
-var sendStateDebounced = throttle(sendState, 2000);
-
-var stateChangeObserver = new WebKitMutationObserver(function(mutations, observer) {
-  sendStateDebounced();
-});
-
-stateChangeObserver.observe(document.getElementById("player"), {attributes: true, characterData: true, subtree:true});
-

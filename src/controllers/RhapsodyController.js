de9d2061045c4f760e58364e6c9c8d9ec38e9bf9
Michael Feldstein <msfeldstein@gmail.com>
2015-08-03 09:23:40 -0700

    Fix napster

--- src/controllers/RhapsodyController.js
+++ src/controllers/RhapsodyController.js
@@ -1,3 +1,26 @@
+throttle = function(fn, threshhold, scope) {
+  threshhold || (threshhold = 250);
+  var last,
+      deferTimer;
+  return function () {
+    var context = scope || this;
+
+    var now = +new Date,
+        args = arguments;
+    if (last && now < last + threshhold) {
+      // hold on to it
+      clearTimeout(deferTimer);
+      deferTimer = setTimeout(function () {
+        last = now;
+        fn.apply(context, args);
+      }, threshhold);
+    } else {
+      last = now;
+      fn.apply(context, args);
+    }
+  };
+}
+
 document.body.addEventListener('DOMSubtreeModified', throttle(sendState, 250));
 
 controller = new BasicController({

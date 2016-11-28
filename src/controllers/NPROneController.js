2ec8e512975bdb10c5d024a7028dc4977d2945fe
Jason Kleinberg <ustice@gmail.com>
2016-11-28 13:31:25 -0500

    Update NPR One controller with new format version

--- src/controllers/NPROneController.js
+++ src/controllers/NPROneController.js
@@ -1,18 +1,26 @@
 controller = new BasicController({
-  supports: {
+  supports: { 
     playpause: true,
     next: true,
-    previous: true
+    previous: true,
+    thumbsUp: true,
   },
-  playStateSelector: '.jp-play',
-  playSelector: '.jp-play',
-  pauseSelector: '.jp-pause',
-  nextSelector: '.jp-next',
-  previousSelector: '.jp-back',
-  titleSelector: '.title',
-  artistSelector: '.slug'
+  playStateSelector: 'toggle-play .player__play-control__icon',
+  playStateClass: '.player__play-control__icon--play', 
+  playPauseSelector: 'toggle-play>button.player__play-control', 
+  nextSelector: 'skip>button.player__play-control', 
+  previousSelector: 'rewind>.player__play-control',
+  titleSelector: 'h1.card__title', 
+  artistSelector: '.card__meta [itemprop="name"]', 
+  artworkImageSelector: '#primaryImage>img', 
+  thumbsUpSelector: 'interesting-button>button', 
+  isThumbsUpSelector: 'interesting-button>button.card__button--active', 
 });
 
 controller.override('isPlaying', function() {
-    return document.querySelector(this.pauseSelector).style.display == 'block';
+  var isPaused = !!document.querySelector('.player__play-control__icon--play');
+  var isLoading = !!document.querySelector('.player__play-control__icon--loading');
+  var isPlaying = !!document.querySelector('.player__play-control__icon--pause');
+
+  return isPaused ? false : isLoading || isPlaying;
 });

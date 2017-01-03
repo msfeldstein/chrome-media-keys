c774f05c431a2afa99edfd3ffa25acceeaf3f602
Peter Minin <peterminin@gmail.com>
2017-01-03 15:13:53 +0300

    Update the VK controller for the new design

--- src/controllers/VkController.js
+++ src/controllers/VkController.js
@@ -4,11 +4,40 @@ controller = new BasicController({
         next: true,
         previous: true
     },
-    playPauseSelector: '#ac_play',
-    previousSelector: '.prev.ctrl',
-    nextSelector: '.next.ctrl',
-    titleSelector: '#ac_title',
-    artistSelector: '#ac_performer',
-    playStateSelector: '#ac_play',
-    playStateClass: 'playing'
-});
+    playPauseSelector: '.top_audio_player_play',
+    previousSelector: '.top_audio_player_prev',
+    nextSelector: '.top_audio_player_next',
+    titleSelector: '.audio_page_player_title_song',
+    artistSelector: '.audio_page_player_title_performer',
+    playStateSelector: '#top_audio_player',
+    playStateClass: 'top_audio_player_playing'
+})
+
+var artistTitleSelector = '.top_audio_player_title'
+var artistTitleRegex = /(.*) – (.*)/
+
+controller.override('getTitle', function(_super) {
+    var title = _super()
+    if (title) {
+        var prefix = ' – '
+        if (title.startsWith(prefix))
+            title = title.substr(prefix.length)
+    } else {
+        var artistTitle = this.querySelectorText(artistTitleSelector)
+        var match = artistTitleRegex.exec(artistTitle)
+        if (match)
+            title = match[1]
+    }
+    return title
+})
+
+controller.override('getArtist', function(_super) {
+    var artist = _super()
+    if (!artist) {
+        var artistTitle = this.querySelectorText(artistTitleSelector)
+        var match = artistTitleRegex.exec(artistTitle)
+        if (match)
+            artist = match[2]
+    }
+    return artist
+})

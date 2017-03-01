a6ce6a77bac86fef9bad872b77fa1a1f75b46510
Bibodha Neupane <bibodha@gmail.com>
2017-02-28 18:05:12 -0700

    Refactored the config

--- src/controllers/TidalController.js
+++ src/controllers/TidalController.js
@@ -7,18 +7,18 @@ var config = {
   useLazyObserving: true,
   playStateClass: 'play-controls__main-button--playing',
   nextSelector: '.icon-Playback_next',
-  previousSelector: '.icon-Playback_previous'
+  previousSelector: '.icon-Playback_previous',
+  artworkImageSelector: '.player__image-container__image',
+  frameSelector: '.player__wrapper',
+  playStateSelector: '.play-controls__main-button--playing',
+  playSelector: '.play-controls__play',
+  pauseSelector: '.play-controls__pause',
+  titleSelector: '.player__heading',
+  artistSelector: '.player__links'
 }
 
-config.artworkImageSelector = '.player__image-container__image';
-config.frameSelector = '.player__wrapper';
-config.playStateSelector = '.play-controls__main-button--playing';
-config.playSelector = '.play-controls__play';
-config.pauseSelector = '.play-controls__pause';
-config.titleSelector = '.player__heading';
-config.artistSelector = '.player__links';
-
 controller = new BasicController(config);
+
 controller.override('getAlbumArt', function () {
   var img = this.doc().querySelector(this.artworkImageSelector);
   return img && img.src.replace(/\d+x\d+/, "640x640");

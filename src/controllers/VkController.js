c3921e003e6646fed7b791ed65f126429a9a5535
Peter Minin <peterminin@gmail.com>
2017-05-01 19:24:09 +0300

    Add support for album arts on VK

--- src/controllers/VkController.js
+++ src/controllers/VkController.js
@@ -9,6 +9,7 @@ controller = new BasicController({
     nextSelector: '.top_audio_player_next',
     titleSelector: '.audio_page_player_title_song',
     artistSelector: '.audio_page_player_title_performer',
+    artworkImageSelector: '.audio_page_player__cover',
     playStateSelector: '#top_audio_player',
     playStateClass: 'top_audio_player_playing'
 })
@@ -41,3 +42,12 @@ controller.override('getArtist', function(_super) {
     }
     return artist
 })
+
+controller.override('getAlbumArt', function() {
+    var img = document.querySelector(this.artworkImageSelector)
+    if (img) {
+        var style = img.currentStyle || window.getComputedStyle(img, false)
+        return style.backgroundImage.slice(5, -2)
+    }
+    return undefined
+});

cb6afd96a98441d155f0f1982d0f57f5ddbc5cef
Yoann FROMMELT <yfrommelt@hevaweb.com>
2017-07-26 11:12:07 +0200

    Add thumbs actions for Google Music

--- src/controllers/GoogleMusicController.js
+++ src/controllers/GoogleMusicController.js
@@ -1,27 +1,31 @@
 controller = new BasicController({
-  supports: {
-    playpause: true,
-    next: true,
-    previous: true
-  },
-  playStateSelector: '[data-id=play-pause]',
-  playStateClass: 'playing',
-  playPauseSelector: '[data-id=play-pause]',
-  nextSelector: '[data-id=forward]',
-  previousSelector: '[data-id=rewind]',
-  titleSelector: '#currently-playing-title',
-  artistSelector: '#player-artist',
-  artworkImageSelector: '#playerBarArt'
+    supports: {
+        playpause: true,
+        next: true,
+        previous: true,
+        thumbsUp: true,
+        thumbsDown: true,
+    },
+    playStateSelector: '[data-id=play-pause]',
+    playStateClass: 'playing',
+    playPauseSelector: '[data-id=play-pause]',
+    nextSelector: '[data-id=forward]',
+    previousSelector: '[data-id=rewind]',
+    titleSelector: '#currently-playing-title',
+    artistSelector: '#player-artist',
+    artworkImageSelector: '#playerBarArt',
+    thumbsUpSelector: 'paper-icon-button[data-rating="5"]',
+    thumbsDownSelector: 'paper-icon-button[data-rating="1"]',
 });
 
-controller.override('init', function(_super) {
-  if (document.querySelector(this.playStateSelector))
-    return _super();
-  else
-    return false;
+controller.override('init', function (_super) {
+    if (document.querySelector(this.playStateSelector))
+        return _super();
+    else
+        return false;
 });
 
-controller.override('getAlbumArt', function(_super) {
-  var art = _super();
-  return art && art.replace('s130', 's300');
+controller.override('getAlbumArt', function (_super) {
+    var art = _super();
+    return art && art.replace(/=s\d+-/, '=s300-');
 });

4d149a11b3057731979a35973272d484ecced0c7
Slawomir Wdowka <swdowka@gmail.com>
2020-02-16 01:42:04 +0100

    Update Last.fm controller to support current player after redesign. (#133)

--- src/controllers/LastfmController.js
+++ src/controllers/LastfmController.js
@@ -1,52 +1,58 @@
 controller = {
-    init: function() {
-      document.querySelector("#radioTrackMeta").addEventListener('DOMSubtreeModified', sendState);
-      return true;
-    },
-    name: "Last.fm",
-    sendStateSoon: function() {
-      var title = this.getState().title;
-      function keepChecking() {
-        if (controller.getState().title != title) {
-          sendState();
-        } else {
-          setTimeout(keepChecking, 300);
-        }
-      }
-      setTimeout(keepChecking, 300);
-    },
-    nextSong: function() {
-      clickById('radioControlSkip');
-      this.sendStateSoon();
-    },
-    play: function() {
-      if (this.isPlaying()) {
-        clickById('radioControlPause');
+  init: function() {
+    document.querySelector('.player-bar-now-playing').addEventListener('DOMSubtreeModified', sendState);
+    return true;
+  },
+  name: "Last.fm",
+  sendStateSoon: function() {
+    var title = this.getState().title;
+    function keepChecking() {
+      if (controller.getState().title != title) {
+        sendState();
       } else {
-        clickById('radioControlPlay');
+        setTimeout(keepChecking, 300);
       }
-      this.sendStateSoon();
-    },
-    isPlaying: function() {
-      return webRadio && webRadio.classList.contains('playing');
-    },
-    favorite: function() {
-      clickById('radioControlLove');
-      this.sendStateSoon();
-    },
-    supports: {
-        next:true,
-        favorite: true,
-        playpause:true
-    },
-    getState: function() {
-        var state = {};
-        state.artist = querySelectorText("#radioTrackMeta .artist");
-        state.title = querySelectorText("#radioTrackMeta .track");
-        var art =  document.querySelector('.metadataHeader img');
-        state.albumArt = art && art.src;
-        state.favorite = radioPlayer.classList.contains('loved');
-        state.playing = this.isPlaying();
-        return state;
     }
+    setTimeout(keepChecking, 300);
+  },
+  nextSong: function() {
+    clickBySelector('.media-controls .js-next.player-bar-btn');
+    this.sendStateSoon();
+  },
+  previuosSong: function() {
+    clickBySelector('.media-controls .js-previous.player-bar-btn');
+    this.sendStateSoon();
+  },
+  play: function() {
+    clickBySelector('.media-controls .js-play-pause.player-bar-btn');
+    this.sendStateSoon();
+  },
+  isPlaying: function() {
+    const playButton = document.querySelector('.media-controls .js-play-pause.player-bar-btn');
+    return playButton && playButton.classList.contains('player-bar-btn--pause')
+  },
+  isLoved: function() {
+    const loveButton = document.querySelector('.media-controls .player-bar-btn.player-bar-btn--love');
+    return loveButton && loveButton.classList.contains('player-bar-btn--loved');
+  },
+  favorite: function() {
+    clickBySelector('.media-controls .player-bar-btn.player-bar-btn--love')
+    this.sendStateSoon();
+  },
+  supports: {
+    next: true,
+    previous: true,
+    favorite: true,
+    playpause: true
+  },
+  getState: function() {
+    const art =  document.querySelector('.player-bar-artwork.js-artwork img');
+    return {
+      artist: querySelectorText('.player-bar-artist-name'),
+      title: querySelectorText('.player-bar-track-name'),
+      albumArt: art && art.src,
+      favorite: this.isLoved(),
+      playing: this.isPlaying()
+    };
+  }
 }
